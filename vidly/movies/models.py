2eb207bba1e51d3bf19d623d913b8b4c8e56a8ed
Mo Sadiq <moesadique@gmail.com>
2020-10-29 09:58:43 +0530

    Movies Model Setup

--- vidly/movies/models.py
+++ vidly/movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+class Movies(models.Model):
+    title = models.CharField(max_length=50)
+    rent = models.IntegerField()
+    stock = models.IntegerField()
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
